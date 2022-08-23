ce8d442943dd63bd78c2d0d50205c78860824043
hj <hanjun@yiye.ai>
2022-08-23 15:33:13 +0800

    🎨

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>
+  <h1 class="ml-3">
     {{ msg }}
   </h1>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import autoprefixer from "autoprefixer";
 import postcssNesting from "postcss-nesting";
+import Unocss from "unocss/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Unocss()],
   server: {
     host: true,
   },
