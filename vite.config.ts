0f1d07b205ecde1ec1c68abb8e93cc671288ac9e
hj <hanjun@yiye.ai>
2022-08-23 14:08:08 +0800

    ✨ css插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import autoprefixer from "autoprefixer";
+import postcssNesting from "postcss-nesting";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer, postcssNesting],
+    },
   },
 });
