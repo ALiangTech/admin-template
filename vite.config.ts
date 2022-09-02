913867a94fbd8ce6087ffa30630fe0f8e570a282
hj <hanjun@yiye.ai>
2022-09-02 14:04:11 +0800

    🎨

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig, mergeConfig } from "vite";
 import baseConfig from "./config/vite/base";
 export default defineConfig(async ({ command }) => {
-  const fetchDevConfig = () => import(`./config/vite/${command}`);
-  const config = await fetchDevConfig();
+  let config = null;
+  if (command === "serve") {
+    const fetchServeConfig = () => import("./config/vite/serve.js");
+    config = await fetchServeConfig();
+  } else {
+    const fetchBuildConfig = () => import("./config/vite/build.js");
+    config = await fetchBuildConfig();
+  }
   return mergeConfig(baseConfig, {
     ...config.default,
   });
