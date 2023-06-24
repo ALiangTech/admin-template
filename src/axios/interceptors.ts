e6c7b00c9c7f567bf304afc355857b0037f52f54
AliangTech <1305544590@qq.com>
2023-06-24 22:24:37 +0800

    🎨 更新项目规范

--- src/axios/interceptors.ts
+++ src/axios/interceptors.ts
@@ -4,11 +4,11 @@ import { useBackToLogin } from "@/hooks";
 // 请求拦截器
 export const useRequestIntercept = (instance: AxiosInstance) => {
   instance.interceptors.request.use(
-    function (config) {
+    (config) => {
       // 在发送请求之前做些什么
       return config;
     },
-    function (error) {
+    (error) => {
       // 对请求错误做些什么
       return Promise.reject(error);
     }
@@ -28,12 +28,12 @@ const logout = (response: AxiosResponse) => {
 
 export const useResponseIntercept = (instance: AxiosInstance) => {
   instance.interceptors.response.use(
-    function (response) {
+    (response) => {
       // 2xx 范围内的状态码都会触发该函数。
       // 对响应数据做点什么
       return response;
     },
-    function (error) {
+    (error) => {
       logout(error);
       // 超出 2xx 范围的状态码都会触发该函数。
       // 对响应错误做点什么
