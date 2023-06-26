a42185339e18e71931f360cc8ef63c7eb019a340
AliangTech <1305544590@qq.com>
2023-06-26 22:55:38 +0800

    🎨 修改unocss配置

--- config/vite/plugins/visualizer.ts
+++ config/vite/plugins/visualizer.ts
@@ -1,5 +1,5 @@
 import { visualizer } from "rollup-plugin-visualizer";
-import { type PluginOption } from "vite";
+import type { PluginOption } from "vite";
 export default function visualizerPlugin() {
   return visualizer({
     filename: "./node_modules/.cache/visualizer/stats.html",
