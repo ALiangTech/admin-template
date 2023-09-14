0ba869db5518ba00a5089db5e3252cc0f0e49b26
平平无奇的阿良 <40046221+ALiangTech@users.noreply.github.com>
2023-09-14 10:45:20 +0800

    🏷️ 添加自定义router-metats类型

--- src/routers/typings.d.ts
+++ src/routers/typings.d.ts
@@ -1,10 +1,10 @@
-// import "vue-router";
-// interface Menu {
-//   label: string;
-// }
-// declare module "vue-router" {
-//   interface RouteMeta {
-//     code: string;
-//     menu: Menu;
-//   }
-// }
+import "vue-router";
+interface Menu {
+  label: string;
+}
+declare module "vue-router" {
+  interface RouteMeta {
+    code: string;
+    menu: Menu;
+  }
+}
