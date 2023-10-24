9bdd8d086f7c1897e4db1fd35d6170040bdb8b58
AliangTech <seminapater@gmail.com>
2023-10-24 13:51:07 +0800

    断言

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <ZSliceMenus :route-name="users.name as string"></ZSliceMenus>
+  <ZSliceMenus :route-name="routeName"></ZSliceMenus>
 </template>
 
 <script setup lang="ts">
 import ZSliceMenus from "@/components/z-slide-menus/index.vue";
 import { users } from "./router";
+const routeName = users.name as string;
 </script>
